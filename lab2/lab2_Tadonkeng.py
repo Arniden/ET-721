934a5d91c20002bce710925be20cace00865fb82
Arnaud Tadonkeng <atadonkeng@gmail.com>
2026-09-02 15:11:18 -0400

    Add lab2 Python file

--- lab2/lab2_Tadonkeng.py
+++ lab2/lab2_Tadonkeng.py
@@ -1,5 +1,11 @@
 """
 Arnaud Tadonkeng
 Lab 2: Git and GitHub repository
+Sep 3, 2025
 """
+username = input("Enter a username: ")
 print(f"Welcome to Git commands {username}")
+
+# new information
+num = 8
+print(f"Double of number {num} is {num*2}")
